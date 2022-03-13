a7d4d68bae4c4b29d5a8fb5ab41b5b0f72379af3
QuentinRoa1 <roaq1@mymail.nku.edu>
2022-03-13 16:17:42 -0400

    fixed spelling error
    fixed spelling error and added comments to script.js, specifically about what needs to be done.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
+// can change names if you think it is confusing, not implemented yet.
+// grabs array at index and loads into a card.
 function loadcard(index){
-
+// needs work
 }
+// uses loadcard() to load a specific number of cards, can change to load from newest by using length of array of arrays - i.
 function loadcards(numOfCards){
+    // should work once loadcard() is complete.
     for(i=0;i<numOfCards;i++){
         loadcard(i);
     }
