3ae2f616a6cf51a708dd8b27cc381488cb4dadc1
QuentinRoa1 <roaq1@mymail.nku.edu>
2022-03-13 18:17:05 -0400

    Believe to have made a working cardloader()
    Keyword believe, have not tested because we have no sample data yet, maybe we should add an array parameter to these to allow us to use it in loading the arrays for the saved menu as well, it asks for an index so we could change the loadcards() function to load based on the id gotten form the index if it is on the users saved page.

--- js/script.js
+++ js/script.js
@@ -17,22 +17,37 @@ function loadcard(index){
     divfooter.classList.add("card-footer p-4 pt-0 border-top-0 bg-transparent");
     divcenter2=document.createElement("div");
     divcenter2.classList.add("text-center");
-    Aplay=document.createElement("a");
+    Aplay=document.createElement("a").href("page.html?user=${userID}&cast=${podcastID}");
     Aplay.classList.add("btn btn-outline-dark mt-auto mx-2");
-    Asave=document.createElement("a");
+    Asave=document.createElement("a").href("play.html?cast=${podcastID}");
     Asave.classList.add("btn btn-outline-dark mt-auto mx-2");
-    text=document.createTextNode("Save")
+    //page is acting as users homepage, can be changed. passing userID as user and podcastID as cast to be caught and added to the users saved podcast array.
+    text=document.createTextNode("Save");
     Asave.appendChild(text);
-    text=document.createTextNode("Play")
+    //play is being used as the podcast page, can be changed. podcastID as cast being passed to be caught and used to load the podcast.
+    text=document.createTextNode("Play");
     Aplay.appendChild(text); 
     divcenter2.appendChild(Asave);
     divcenter2.appendChild(Aplay); 
+    divfooter.appendChild(divcenter2);
+    text=document.createTextNode(authorName);
+    pname.appendChild(text);
+    text=document.createTextNode(podtitle);
+    title.appendChild(text);
+    divcenter.appendChild(title);
+    divcenter.appendChild(pname);
+    divbody.appendChild(divcenter);
+    divcard.appendChild(divbody);
+    divcard.appendChild(divfooter);
+    divcol.appendChild(divcard);
+    // should put in the right place in the document.
+    document.body.section.appendChild(divcol);
 }
-// uses loadcard() to load a specific number of cards, can change to load from newest by using length of array of arrays - i.
+// uses loadcard() to load a specific number of cards, can change to load from newest by using length of array of arrays - i-page*numOfCards.
 function loadcards(numOfCards){
     // should work once loadcard() is complete.
-    for(i=0;i<numOfCards;i++){
-        loadcard(i);
+    for(i=0;i<numOfCards||array.length-page*numOfCards<0;i++){
+        loadcard(array.length-i-page*numOfCards);
     }
 }
 /*
