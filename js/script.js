c3961a182eeb00136c3e8de8d83bb72d5e28d1bb
QuentinRoa1 <roaq1@mymail.nku.edu>
2022-03-16 19:16:30 -0400

    Added stuff
    working loadcards function, aded nav to profile, added dropdown to index, needs to be added to other pages.

--- js/script.js
+++ js/script.js
@@ -1,53 +1,55 @@
 // can change names if you think it is confusing, not implemented yet.
 // grabs array at index and loads into a card.
-function loadcard(index){
+const array=[{podtitle:"Hello", podcastID:"0",authorName:"Berry"},{podtitle:"Hello", podcastID:"0",authorName:"Berry"}];
+const userID="3";
+const page=0;
+function loadcard(i){
     //needs work
-    divcol=document.createElement("div");
-    divcol.classList.add("col mb5");
-    divcard=document.createElement("div");
-    divcard.classList.add("card h-50");
-    divbody=document.createElement("div");
-    divbody.classList.add("card-body p-4");
-    divcenter=document.createElement("div");
+    var divcol=document.createElement('div');
+    divcol.classList.add("col","mb5","border","rounded","m-2");
+    var divcard=document.createElement('div');
+    divcard.classList.add("card", "h-50","border-0");
+    var divbody=document.createElement('div','border-0');
+    divbody.classList.add("card-body", "p-4");
+    var divcenter=document.createElement('div');
     divcenter.classList.add("text-center");
-    title=document.createElement("h5");
+    var title=document.createElement('h5');
     title.classList.add("fw-bolder");
-    pname=document.createElement("p");
-    divfooter=document.createElement("div");
-    divfooter.classList.add("card-footer p-4 pt-0 border-top-0 bg-transparent");
-    divcenter2=document.createElement("div");
+    var pname=document.createElement('p');
+    var divfooter=document.createElement('div');
+    divfooter.classList.add("card-footer","p-4", "pt-0", "border-top-0", "bg-transparent");
+    var divcenter2=document.createElement('div');
     divcenter2.classList.add("text-center");
-    Aplay=document.createElement("a").href("page.html?user=${userID}&cast=${podcastID}");
-    Aplay.classList.add("btn btn-outline-dark mt-auto mx-2");
-    Asave=document.createElement("a").href("play.html?cast=${podcastID}");
-    Asave.classList.add("btn btn-outline-dark mt-auto mx-2");
+    var Asave=document.createElement('a');
+    Asave.classList=("btn btn-outline-dark mt-auto mx-2");
+    var Aplay=document.createElement('a');
+    Aplay.classList=("btn btn-outline-dark mt-auto mx-2");
     //page is acting as users homepage, can be changed. passing userID as user and podcastID as cast to be caught and added to the users saved podcast array.
-    text=document.createTextNode("Save");
-    Asave.appendChild(text);
-    //play is being used as the podcast page, can be changed. podcastID as cast being passed to be caught and used to load the podcast.
-    text=document.createTextNode("Play");
-    Aplay.appendChild(text); 
-    divcenter2.appendChild(Asave);
+    Asave.textContent='Save';
+    Aplay.textContent='Play';
+    //play is being used as the podcast page, can be changed. podcastID as cast being passed to be caught and used to load the podcast. 
+    divcenter2.append(Asave);
     divcenter2.appendChild(Aplay); 
-    divfooter.appendChild(divcenter2);
-    text=document.createTextNode(authorName);
-    pname.appendChild(text);
-    text=document.createTextNode(podtitle);
-    title.appendChild(text);
-    divcenter.appendChild(title);
-    divcenter.appendChild(pname);
-    divbody.appendChild(divcenter);
-    divcard.appendChild(divbody);
-    divcard.appendChild(divfooter);
-    divcol.appendChild(divcard);
+    divfooter.append(divcenter2);
+    pname.textContent=array[i].authorName;
+    title.textContent=array[i].podtitle;
+    divcenter.append(title);
+    divcenter.append(pname);
+    divbody.append(divcenter);
+    divcard.append(divbody);
+    divcard.append(divfooter);
+    divcol.append(divcard);
     // should put in the right place in the document.
-    document.body.section.appendChild(divcol);
+    var put=document.getElementById('cards-here');
+    put.append(divcol);
 }
 // uses loadcard() to load a specific number of cards, can change to load from newest by using length of array of arrays - i-page*numOfCards.
 function loadcards(numOfCards){
     // should work once loadcard() is complete.
-    for(i=0;i<numOfCards||array.length-page*numOfCards<0;i++){
-        loadcard(array.length-i-page*numOfCards);
+    for(var i=0;i<numOfCards;i++){
+        var temp=numOfCards-i-1;
+        console.log(i);
+        loadcard(i);
     }
 }
 /*
