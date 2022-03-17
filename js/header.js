83af3972e8a2b245e9bae300ffc7f382d08a4f7e
Dylan <99437817+gainesd2@users.noreply.github.com>
2022-03-16 20:02:34 -0400

    Authentication and a lot of other stufff
    Index.html changes
    
    header updated, header.js is used to show/hide buttons
    
    authentication (sign up, sign in, sign out) now work, not much validation

---
+++ js/header.js
@@ -0,0 +1,61 @@
+// checks if a user is signed in, returns true if so
+function bouncer() {
+    if (window.sessionStorage.getItem("email")) {
+        if (window.sessionStorage.getItem("auth") == "true") {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function buttonLoader() {
+    if (bouncer()) {
+        console.log("ping")
+            // ACCOUNT INFO
+        let accountBtn = document.createElement("a")
+        accountBtn.classList.add("btn", "btn-sm", "btn-outline-dark", "bi", "bi-person-fill");
+        accountBtn.href = "profile.html";
+        accountBtn.innerHTML += " Profile";
+        /* // SAVED -- hidden until implementation
+        let savedBtn = document.createElement("a")
+        savedBtn.classList.add("btn", "btn-sm", "btn-outline-dark", "bi", "bi-bookmark-fill");
+        savedBtn.href = "saved.html";
+        savedBtn.innerHTML += " Saved"; */
+
+        // BILLING
+        let billingBtn = document.createElement("a")
+        billingBtn.classList.add("btn", "btn-sm", "btn-outline-dark", "bi", "bi-currency-dollar");
+        billingBtn.addEventListener("click", () => {
+            alert("not implemented yet");
+        });
+        billingBtn.innerHTML += " Billing";
+
+        // LOGOUT
+        let signOutBtn = document.createElement("a")
+        signOutBtn.classList.add("btn", "btn-sm", "btn-outline-dark", "bi", "bi-key-fill");
+        signOutBtn.href = "./sign/out.html";
+        signOutBtn.innerHTML += " Sign Out";
+
+        btnHoldingLad.appendChild(accountBtn)
+            // hidden until implementation
+            //btnHoldingLad.appendChild(savedBtn)
+        btnHoldingLad.appendChild(billingBtn)
+        btnHoldingLad.appendChild(signOutBtn)
+            //if signed in, append a series of links
+    } else {
+        console.log("ping")
+        let signInBtn = document.createElement("a")
+        signInBtn.classList.add("btn", "btn-sm", "btn-outline-dark");
+        signInBtn.href = "./sign/in.html";
+        signInBtn.innerHTML += "Sign In";
+
+        let signUpBtn = document.createElement("a")
+        signUpBtn.classList.add("btn", "btn-sm", "btn-outline-dark");
+        signUpBtn.href = "./sign/up.html";
+        signUpBtn.innerHTML += "Sign Up";
+
+        btnHoldingLad.appendChild(signInBtn)
+        btnHoldingLad.appendChild(signUpBtn)
+    }
+}
