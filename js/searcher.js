ffa2d45a4f4dc298c4dd2c2c6644d3ee5021403e
QuentinRoa1 <roaq1@mymail.nku.edu>
2022-03-24 23:19:49 -0400

    Working Search bar
    The search bar is rudimentary, but working, I do plan on changing it later, but you can search things up, if we really wanted we could get rid of the search page, and implement this into the index page. This does need a large amount of work, I  did add a get method to the search bar to use. the search algorithm  is all currently sitting inside the searcher.js's ajax function. I had to make it work ,  later we can make it better.

--- js/searcher.js
+++ js/searcher.js
@@ -19,26 +19,90 @@ function searchLoader() {
             /* will return an array of objects formatted like like this:
                 [
                     {
-                        "ID: : 1,
-                        "DOI": "1234561234",
-                        "fName": "Dylan",
-                        "lName": "Gaines",
-                        "email": "gainesd2@nku.edu",
-                        "title": "This is a Test Article",
-                        "journal": "The Test Journal",
-                        "category": 0,
-                        "tags": ["Test", "Zamn", "Cool Kids"],
-                        "filePlaceHolder": "File Man",
-                        "ratings": [[0, 5], [1,4]]
-                    }
+    "ID": 1,
+    "uploaderID": 0,
+    "DOI": "1234561234",
+    "title": "This is a Test Article",
+    "journal": "The Test Journal",
+    "category": 0,
+    "tags": [
+      "Test",
+      "Zamn",
+      "Cool Kids"
+    ],
+    "filePlaceHolder": "File Man",
+    "ratings": [
+      [
+        0,
+        5
+      ]
+    ],
+    "publishers": "me, myself, I",
+    "pubDate": "3/17/2022"
+  }
                 ]
             */
             //
+            var temp = []; // used to store temp array
+            var temp2 = 0; // used to add to temp array
             let cardData = podData;
             let searchTerms = getAllUrlParams().terms;
             // for each card
             // for each term
-
+            // Makes sure no there is a search term.
+            if (searchTerms !== "") {
+                // Goes through each card.
+                cardData.forEach(element => {
+                    // sets all checked to another variable that is all lowercase.
+                    var title = element.title.toLowerCase();
+                    var journal = element.journal.toLowerCase();
+                    var publisher = element.publishers.toLowerCase();
+                    // if and else if clauses to add the card to the temp array. 
+                    if (element.ID == searchTerms) {
+                        // puts the card into the temp array
+                        temp[temp2] = element;
+                        // iterates temp2
+                        temp2++;
+                    }
+                    else if (element.DOI == searchTerms) {
+                        // puts the card into the temp array
+                        temp[temp2] = element;
+                        // iterates temp2
+                        temp2++;
+                    }
+                    else if (title.indexOf(searchTerms) !== -1) {
+                        // puts the card into the temp array
+                        temp[temp2] = element;
+                        // iterates temp2
+                        temp2++;
+                    }
+                    else if (journal.indexOf(searchTerms) !== -1) {
+                        // puts the card into the temp array
+                        temp[temp2] = element;
+                        // iterates temp2
+                        temp2++;
+                    }
+                    else if (publisher.indexOf(searchTerms) !== -1) {
+                        // puts the card into the temp array
+                        temp[temp2] = element;
+                        // iterates temp2
+                        temp2++;
+                    }
+                    else {
+                        element.tags.forEach(ele => {
+                            var tag = ele.toLowerCase();
+                            if (tag.indexOf(searchTerms) !== -1) {
+                                // puts the card into the temp array
+                                temp[temp2] = element;
+                                // iterates temp2
+                                temp2++;
+                            }
+                        });
+                    }
+                });
+                // sets cardData to temp
+                cardData = temp;
+            }
             // send the data to loadCard;
 
             loadcards(cardData);
